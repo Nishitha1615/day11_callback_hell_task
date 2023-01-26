47acbc6d987a261b8429879216c836290d41a6c2
Nishitha1615 <121231573+Nishitha1615@users.noreply.github.com>
2023-01-26 20:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+var data=document.querySelector('h2');
+
+setTimeout(()=>{
+    data.innerText='10'
+    setTimeout(()=>{
+        data.innerText='9'
+        setTimeout(()=>{
+            data.innerText='8'
+            setTimeout(()=>{
+                data.innerText='7'
+                setTimeout(()=>{
+                    data.innerText='6'
+                    setTimeout(()=>{
+                        data.innerText='5'
+                        setTimeout(()=>{
+                            data.innerText='4'
+                            setTimeout(()=>{
+                                data.innerText='3'
+                                setTimeout(()=>{
+                                    data.innerText='2'
+                                    setTimeout(()=>{
+                                        data.innerText='1'
+                                        setTimeout(()=>{
+                                            data.innerText='HAPPY INDEPENDENCE DAY'
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+// setTimeout(()=>{
+//         data.innerText = '5'
+//         setTimeout(()=>{
+//             data.innerText = '4'
+//             setTimeout(()=>{
+//                 data.innerText = '3'
+//                 setTimeout(()=>{
+//                     data.innerText = '2'
+//                     setTimeout(()=>{
+//                         data.innerText = '1'
+//                         setTimeout(()=>{
+//                             data.innerText = '0'
+//                             setTimeout(()=>{
+//                                 data.innerText = 'Happy Independece Day'
+//                             },1000)
+//                         },1000)
+//                     },1000)
+//                 },1000)
+//             },1000)
+//         },1000)
+//     },1000)
